4fcdd7db32d95fa96d4bc129151cffee278b3e58
= <=>
2020-07-08 10:09:12 +0700

    pull3

---
+++ project3/Models/Service/Hashing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Service
+{
+    public class Hashing
+    {
+        public static string GetRandomSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(12);
+        }
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, GetRandomSalt());
+        }
+        public static bool ValidatePassword(string password ,string correctHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, correctHash);
+        }
+        
+    }
+}
---
+++ project3/project3/Models/Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Models.FrameWork;
+
+namespace project3.Models
+{
+    public class Home
+    {
+        public List<Bouquest> bouquests { get; set; }
+        public Customer customer { get; set; }
+    }
+}
